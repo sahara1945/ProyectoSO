9b041dbb920de9cd94a2299d921069a0611caca7
unknown <sahara.gandarillas@gmail.com>
2023-03-29 00:52:51 +0200

    Version_1

---
+++ servidor.c
@@ -0,0 +1,140 @@
+#include <string.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <stdio.h>
+#include <pthread.h>
+#include <mysql.h>
+int main(int argc, char *argv[]) {
+	MYSQL *conn;
+	int err;
+	MYSQL_RES *resultado;
+	MYSQL_ROW row;
+	conn = mysql_init(NULL);
+	if (conn==NULL) {
+		printf ("Error al crear la conexion: %u %s\n", 
+				mysql_errno(conn), mysql_error(conn));
+		exit (1);
+	}
+	conn = mysql_real_connect (conn, "localhost","root", "mysql", "Comando_SQL", 0, NULL, 0);
+	if (conn==NULL) {
+		printf ("Error al inicializar la conexion: %u %s\n", 
+				mysql_errno(conn), mysql_error(conn));
+		exit (1);
+	}
+	
+	int sock_conn, sock_listen, ret;
+	struct sockaddr_in serv_adr;
+	char peticion[512];
+	char respuesta[512];
+	// INICIALITZACIONS
+	// Obrim el socket
+	if ((sock_listen = socket(AF_INET, SOCK_STREAM, 0)) < 0)
+		printf("Error creant socket");
+	// Fem el bind al port
+	
+	
+	memset(&serv_adr, 0, sizeof(serv_adr));// inicialitza a zero serv_addr
+	serv_adr.sin_family = AF_INET;
+	
+	// asocia el socket a cualquiera de las IP de la m?quina. 
+	//htonl formatea el numero que recibe al formato necesario
+	serv_adr.sin_addr.s_addr = htonl(INADDR_ANY);
+	// establecemos el puerto de escucha
+	serv_adr.sin_port = htons(9050);
+	if (bind(sock_listen, (struct sockaddr *) &serv_adr, sizeof(serv_adr)) < 0)
+		printf ("Error al bind");
+	
+	if (listen(sock_listen, 3) < 0)
+		printf("Error en el Listen");
+	
+	int i;
+	// Bucle infinito
+	for (;;){
+		printf ("Escuchando\n");
+		
+		sock_conn = accept(sock_listen, NULL, NULL);
+		printf ("He recibido conexion\n");
+		//sock_conn es el socket que usaremos para este cliente
+		
+		int terminar =0;
+		// Entramos en un bucle para atender todas las peticiones de este cliente
+		//hasta que se desconecte
+		while (terminar ==0)
+		{
+			// Ahora recibimos la petici?n
+			ret=read(sock_conn,peticion, sizeof(peticion));
+			printf ("Recibido\n");
+			
+			// Tenemos que añadirle la marca de fin de string 
+			// para que no escriba lo que hay despues en el buffer
+			peticion[ret]='\0';
+			
+			
+			printf ("Peticion: %s\n",peticion);
+			
+			// vamos a ver que quieren
+			char *p = strtok( peticion, "/");
+			int codigo =  atoi (p);
+			// Ya tenemos el c?digo de la petici?n
+			char nombre[20];
+			char contraseña[20];
+			
+			if (codigo !=0)
+			{
+				p = strtok( NULL, "/");
+				strcpy (nombre, p);
+				
+				p = strtok( NULL, "/");
+				strcpy (contra, p);
+				
+				p = strtok( NULL, "/");
+				strcpy (jugador, p);
+				
+				p = strtok( NULL, "/");
+				partida = atoi (p);
+				
+				printf ("Codigo: %d, Nombre: %s\n", codigo, nombre);
+			}
+			
+			if (codigo ==0) //petici?n de desconexi?n
+				terminar=1;
+			else if (codigo ==1) //mirar si el usuario y contraseÃ±a son correctos
+			{	
+				char consulta [80];
+				strcpy (consulta,"SELECT Usuarios.Contrasena FROM Usuarios WHERE Usuarios.nombre = '");
+				strcat (consulta, nombre);
+				strcat (consulta,"'");
+				
+				err=mysql_query (conn, consulta);
+				
+				if (err!=0) {
+					printf ("Error al consultar datos de la base %u %s\n",
+							mysql_errno(conn), mysql_error(conn));
+					exit (1);
+				}
+				resultado = mysql_store_result (conn);
+				int result= strcmp(resultado, contra);
+				if (result == 0)
+					sprintf (respuesta, "Correcto");
+				else
+					sprintf (respuesta, "Incorrecto");
+				
+			}
+			
+			if (codigo !=0)
+			{
+				
+				printf ("Respuesta: %s\n", respuesta);
+				// Enviamos respuesta
+				write (sock_conn,respuesta, strlen(respuesta));
+			}
+		}
+		// Se acabo el servicio para este cliente
+		close(sock_conn); 
+	}
+	
+}
+
